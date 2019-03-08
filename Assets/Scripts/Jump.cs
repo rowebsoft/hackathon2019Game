ea1db1df8c4dc89b817be8c03434997721060cd2
Ricardo Quintanilha <ricardo.aquintanilha@gmail.com>
2019-03-08 23:47:00 +0100

    Add jump script to the character

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Jump : MonoBehaviour {
+
+	public Vector2 jump;
+	public float jumpForce = 2.0f;
+
+	public float playerSpeed;  //allows us to be able to change speed in Unity
+	public Vector2 jumpHeight;
+
+	void Start()
+	{
+	}
+
+	void Update(){
+		transform.Translate(playerSpeed * Time.deltaTime, 0f, 0f);  //makes player run
+
+		if (Input.GetKeyDown(KeyCode.Space))  //makes player jump
+			GetComponent<Rigidbody2D>().AddForce(jump * jumpForce, ForceMode2D.Impulse);
+		}
+	}
+}
